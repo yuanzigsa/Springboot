f304cfd53e4f90973ea89321203d2eee4c4a0032
yuanzigsa <yuanzigsa@gmail.com>
2022-08-25 21:06:53 +0800

    vue页面整体框架

--- springboot+vue/vue/src/App.vue
+++ springboot+vue/vue/src/App.vue
@@ -3,3 +3,8 @@
     <router-view/>
   </div>
 </template>
+<style>
+#app{
+  height: 100%;
+}
+</style>
--- springboot+vue/vue/src/views/HomeView.vue
+++ springboot+vue/vue/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div style="height: 100%;">
     <el-container style="height: 100%; ">
   <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
+    <el-menu :default-openeds="['1', '3']" style="height:100%">
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-message"></i>导航一</template>
         <el-menu-item-group>
@@ -52,7 +52,7 @@
   </el-aside>
   
   <el-container>
-    <el-header style="text-align: right; font-size: 12px">
+    <el-header style="text-align: right; font-size: 12px;border-bottom: 1px solid #ccc;line-height: 60px">
       <el-dropdown>
         <i class="el-icon-setting" style="margin-right: 15px"></i>
         <el-dropdown-menu slot="dropdown">
