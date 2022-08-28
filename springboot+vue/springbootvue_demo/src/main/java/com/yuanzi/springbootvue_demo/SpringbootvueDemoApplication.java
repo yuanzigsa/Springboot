a44721aa8a904b4d93ee730eec6ea2e3e2b49d6c
yuanzigsa <yuanzigsa@gmail.com>
2022-08-29 00:45:15 +0800

    搭建springboot

--- springboot+vue/springbootvue_demo/src/main/java/com/yuanzi/springbootvue_demo/SpringbootvueDemoApplication.java
+++ springboot+vue/springbootvue_demo/src/main/java/com/yuanzi/springbootvue_demo/SpringbootvueDemoApplication.java
@@ -2,12 +2,20 @@ package com.yuanzi.springbootvue_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootvueDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootvueDemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index(){
+        return "欢迎来到元子的第一个页面";
     }
 
 }
