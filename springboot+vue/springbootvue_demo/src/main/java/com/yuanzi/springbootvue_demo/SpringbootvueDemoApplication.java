2d86b4a32fec16041490c1a5db522bc0348bd989
yuanzigsa <yuanzigsa@gmail.com>
2022-08-29 00:06:16 +0800

    springboot初始化

---
+++ springboot+vue/springbootvue_demo/src/main/java/com/yuanzi/springbootvue_demo/SpringbootvueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuanzi.springbootvue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootvueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootvueDemoApplication.class, args);
+    }
+
+}
---
+++ springboot+vue/springbootvue_demo/src/test/java/com/yuanzi/springbootvue_demo/SpringbootvueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanzi.springbootvue_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootvueDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
