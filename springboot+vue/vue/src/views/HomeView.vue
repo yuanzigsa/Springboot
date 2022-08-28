330e579ae4ea01ce34f0d96cf38f0491206c2bd6
yuanzigsa <yuanzigsa@gmail.com>
2022-08-28 22:38:23 +0800

    前端基本雏形

--- springboot+vue/vue/src/views/HomeView.vue
+++ springboot+vue/vue/src/views/HomeView.vue
@@ -94,7 +94,7 @@
           <el-table-column prop="address" label="地址">
           </el-table-column>
           <el-table-column label="操作"  width="200" align="center">
-            <template slot-scope="scope">
+            <template slot-scope="">
               <el-button type="success">编辑 <i class="el-icon-edit"></i></el-button>
               <el-button type="danger">删除 <i class="el-icon-remove-outline"></i></el-button>
             </template>
